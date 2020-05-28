8d4e4f53465f8bf758093b95b25072bb147fb7d8
Leon Barnard <leon@balsamiq.com>
2020-05-28 14:15:49 -0700

    Adding photo gallery side project

---
+++ gallery/index.js
@@ -0,0 +1,33 @@
+let contentElement = document.getElementById('gallery');
+
+function fetchAndGetPhotos() {
+  const responsePromise = fetch('https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=17724440c19a7f2276c841462e832525&photoset_id=72157622614684313&user_id=11126789%40N08&format=json&nojsoncallback=1')
+  responsePromise.then((response) => {
+      return response.json();
+    })
+    .then((responseObject) => {
+      let photos = responseObject.photoset.photo;
+      for (var photo of photos) {
+        let imgItem = document.createElement('img');
+        let captionItem = document.createElement('figcaption');
+        let figureItem = document.createElement('figure');
+        let photoID = photo.id;
+        let photoDesc = photo.title;
+        let photoFarm = photo.farm;
+        let photoServerID = photo.server;
+        let photoSecret = photo.secret;
+        imgItem.setAttribute("src", `https://farm${photoFarm}.staticflickr.com/${photoServerID}/${photoID}_${photoSecret}.jpg`);
+        imgItem.setAttribute("alt", photoDesc);
+        imgItem.setAttribute("title", photoDesc);
+        captionItem.innerHTML = photoDesc;
+        figureItem.appendChild(imgItem);
+        figureItem.appendChild(captionItem);
+        contentElement.appendChild(figureItem);
+      }
+
+  });
+
+} 
+
+fetchAndGetPhotos();
+
---
+++ site/content/fun/photos.md
@@ -0,0 +1,12 @@
+---
+title:  "photos"
+description: "Some of my favorite photos"
+---
+
+[&lt; back to Fun](/fun/)
+
+# Photos
+
+Some of my favorite photos, mostly from my travels.
+
+<div id="gallery"></div>
